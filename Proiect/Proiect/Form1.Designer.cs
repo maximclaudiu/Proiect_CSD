e6f57666f731e0f639cadc758c30ec5828239803
Claudiu <claudiu.maximus99@gmail.com>
2022-03-26 17:30:08 +0200

    Interactiuni DB

--- Proiect/Proiect/Form1.Designer.cs
+++ Proiect/Proiect/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace Proiect
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Connect_B = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Select_B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Connect_B
+            // 
+            this.Connect_B.Location = new System.Drawing.Point(298, 287);
+            this.Connect_B.Name = "Connect_B";
+            this.Connect_B.Size = new System.Drawing.Size(89, 32);
+            this.Connect_B.TabIndex = 0;
+            this.Connect_B.Text = "Connect";
+            this.Connect_B.UseVisualStyleBackColor = true;
+            this.Connect_B.Click += new System.EventHandler(this.Connect_B_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 152);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Select_B
+            // 
+            this.Select_B.Location = new System.Drawing.Point(416, 287);
+            this.Select_B.Name = "Select_B";
+            this.Select_B.Size = new System.Drawing.Size(89, 32);
+            this.Select_B.TabIndex = 0;
+            this.Select_B.Text = "Select";
+            this.Select_B.UseVisualStyleBackColor = true;
+            this.Select_B.Click += new System.EventHandler(this.Select_B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Select_B);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Connect_B);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Connect_B;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Select_B;
     }
 }
 
--- Proiect/Proiect/Form1.cs
+++ Proiect/Proiect/Form1.cs
@@ -7,15 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Proiect
 {
     public partial class Form1 : Form
     {
+        MySqlConnection con = new MySqlConnection(
+            "server = localhost; userid = root; password =; database = csd_project"
+            );
+        MySqlCommand command;
+        MySqlDataReader reader;
+        string query = "select * from `keys`";
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Connect_B_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Closed)
+                try {
+                    con.Open();
+                    textBox1.Text = "You are now connected";
+                    command = con.CreateCommand();
+                    command.Connection = con;
+                }
+                catch (Exception ex) {
+                    textBox1.Text = ex.Message;
+                }
+            else {
+                con.Close();
+                textBox1.Text = "You are now disconnected";
+            }
+
+        }
+
+        private void Select_B_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                
+                command.CommandText = query;
+                reader = command.ExecuteReader();
+                textBox1.Text = "";
+                while (reader.Read())
+                {
+                    int i = 0;
+                    while ( i <reader.FieldCount)
+                    { 
+                        textBox1.Text += reader[i].ToString()+ " ";
+                        i++;
+                    }
+                    textBox1.Text += "\r\n";
+                }
+                reader.Close();
+            }
+        }
     }
 }
